08494abc578baf17f4fda1ae3133f0581bd30ba7
polo-7 <156938982+polo-7@users.noreply.github.com>
2024-09-01 19:36:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function afficherTexte() {
+    document.getElementById("P").style.display = "block";
+}
+
+// Utiliser setTimeout pour appeler la fonction après 3000 millisecondes (3 secondes)
+setTimeout(afficherTexte, 5000);
+
