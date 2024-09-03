2df3d64feae6072a711260b5b2a00314ec7bb108
polo-7 <156938982+polo-7@users.noreply.github.com>
2024-09-03 13:50:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function afficherTexte() {
 // Utiliser setTimeout pour appeler la fonction après 3000 millisecondes (3 secondes)
 setTimeout(afficherTexte, 5000);
 
+window.onscroll = function() {
+    fixHeader();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function fixHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+}
